28a831a6c79659b339075ca2bf4f64e3668f6474
shashankhr <shashankhr018@gmail.com>
2025-06-05 10:34:07 +0530

    ci/cd integrations

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -89,25 +89,27 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    signingConfigs {
+        create("release") {
+            storeFile = file("release-key.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
     lint {
         checkReleaseBuilds = false
-    }
-    signingConfigs {
-        create("release") {
-            storeFile = file("keystore.jks") // or file("composeApp/keystore.jks")
-            storePassword = System.getenv("KEYSTORE_PASSWORD")
-            keyAlias = System.getenv("KEY_ALIAS")
-            keyPassword = System.getenv("KEY_PASSWORD")
-        }
     }
 }
 
